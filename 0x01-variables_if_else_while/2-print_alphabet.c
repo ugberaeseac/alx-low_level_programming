f086ffb7b8e2b0bb6a7151b9adf62ec8346426ca
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-02-17 15:39:57 -0800

    print aphabelts using putchar()

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry Point
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char aph;
+
+	for (aph = 'a'; aph <= 'z'; aph++)
+	{
+		putchar(aph);
+	}
+	return (0);
+}
