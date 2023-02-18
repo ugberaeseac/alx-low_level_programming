6805553c0d4e31d574c2d6fa23796434719e3e99
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-02-17 17:04:33 -0800

    print single digits of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a = 0;
+	while (a < 10)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
+
