4f33f8e103f96a865c6a10d50c8a2fa70443ace1
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-02-17 17:53:04 -0800

    lower case alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	char c = 'z';
+	
+	while (c <= 'a')
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
