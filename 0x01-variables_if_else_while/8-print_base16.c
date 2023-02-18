aa0e12b530b32694f6bbf326099f058377f5cdac
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-02-17 18:18:38 -0800

    print numbers of hexadecimal in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
