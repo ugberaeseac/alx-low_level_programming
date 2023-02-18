dfd67a5e7358dd26e08ea94cfec135be7805111b
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-02-17 18:32:53 -0800

    print numbers with commas and space between

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	
+	for (a = 0; a < 10; a++)
+	{
+	putchar(a + '0');
+	if (a < 9)
+	{
+		putchar(44);
+		putchar(32);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
