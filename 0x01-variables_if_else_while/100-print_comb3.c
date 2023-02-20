c3d96527dfc35e94726b16467a845056351e02bd
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-02-20 13:13:31 -0800

    possible combination of two numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Entry Point
- * Result: Always (0) Success
+ * Return: Always (0) Success
  */
 int main(void)
 {
 	int a, b, c;
-	a = 0;
 
+	a = 0;
 	while (a < 100)
 	{
 		b = a / 10;
@@ -18,12 +18,11 @@ int main(void)
 		{
 			putchar(b + '0');
 			putchar(c + '0');
-		
-		if (a < 89)
-		{
-			putchar(44);
-			putchar(32);
-		}
+			if (a < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
 		}
 		a++;
 	}
