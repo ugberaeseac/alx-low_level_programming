a77791fa3a6a79b68616f2ed8f2c7549a5b54472
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-02-21 16:10:35 -0800

    check for lower alphabets

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks for a lowercase character
+ * Return 0 if lowercase 1 if otheriwse
+ */
+int _islower(int c)
+{
+	int a;
+
+	c = (c + '0');
+	if c >= 97 && <= 122
+	{
+		a = 0
+	}
+	else
+		a = 1
+return (a);
+
+}
