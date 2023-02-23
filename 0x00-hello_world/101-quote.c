9eb5ffb517b394c575d02521d70c1af1f03dbb9e
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-02-23 12:59:14 -0800

    update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return 1 Always Success
+ */
+int main(void)
+{
+	puts("and that piece of art is usefu"\" \- Dora Korpar, 2015-10-19");
+	
+return (1);
+}
