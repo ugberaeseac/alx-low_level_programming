1e1276cd00682e3bca62e7b69b2de31d60a8c22d
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-03-02 13:16:21 -0800

    function that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes string using rot13
+ * @s: string
+ * Return: string s
+ */
+
+char *rot13(char *s)
+{
+
+	int alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		     'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		     'W', 'X', 'Y', 'Z'};
+	int rot13[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		     'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		     'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		     'J', 'K', 'L', 'M'};
+
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		while (alpha[j] != '\0')
+			i++;
+
+		if (s[i] == alpha[j])
+			s[i] = rot13[j];
+	}
+
+	return (s);
+}
