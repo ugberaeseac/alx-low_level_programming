9b60f4cccc24c5408981241f02df5395339a12ac
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-03-02 06:29:03 -0800

    function that copies a string like strncpy ()

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,11 +15,8 @@ _strncpy(char *dest, char *src, int n)
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
+		for (; i < n; i++)
+			dest[i] = '\0';
 	}
-	while (i < n)
-	{
-		dest[i] = '\0';
-		return (dest);
-		i++;
-	}
+return (dest);
 }
