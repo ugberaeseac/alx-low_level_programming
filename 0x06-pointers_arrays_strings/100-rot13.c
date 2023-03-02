5c43b788c9d7fd9496adb789949a09af9233e2b8
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-03-02 13:26:30 -0800

    function that encodes a string using rot13.

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,12 +9,12 @@
 char *rot13(char *s)
 {
 
-	int alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+	int aph[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
 		     'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
 		     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
 		     'W', 'X', 'Y', 'Z'};
-	int rot13[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+	int rot[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
 		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 		     'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
 		     'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
@@ -25,11 +25,11 @@ char *rot13(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		j = 0;
-		while (alpha[j] != '\0' && s[i] != alpha[j])
+		while (aph[j] != '\0' && s[i] != aph[j])
 			i++;
 
-		if (s[i] == alpha[j])
-			s[i] = rot13[j];
+		if (s[i] == aph[j])
+			s[i] = rot[j];
 	}
 
 	return (s);
