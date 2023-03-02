39976517ad43dc46a0c321747ed916f32851e6e0
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-03-02 05:36:14 -0800

    a function that compares two strings.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,13 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int m;
 
-	for (i = 0; (s1[i] != '\0' && s2[i] != '\0'))
+	for (i = 0; (s1[i] != '\0' || s2[i] != '\0'))
 	{
-		m = (s1[i] - s2[i]);
+		if (s1[i] != s2[i])
+		{
+			return ((s1[i] - s2[i]));
+		}
 	}
-	return (m);
+	return (0);
 }
