a2cef010029de0f5e48254d57a1f212ed442c4b9
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-03-02 11:32:30 -0800

    a function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * leet - encode string into 1337 leet
+ * @s: string to manipulate
+ * Return: string
+ */
+
+char *leet(char *s)
+{
+
+	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+				s[i] = b[j];
+		}
+	}
+
+	return (s);
+}
