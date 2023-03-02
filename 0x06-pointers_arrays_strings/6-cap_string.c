7b5fddf954b0808fea8f4b7ae08a7b52e8fd8b84
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-03-02 14:09:06 -0800

    function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,16 +12,18 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-	for (; str[i] >= 'a' && str[i] <= 'z'; i++)
-		if (str[i - 1] == ' ' || str[i - 1] == '\t'
-		|| str[i - 1] == '\n' || str[i - 1] == ','
-		|| str[i - 1] == ';' || str[i - 1] == '.'
-		|| str[i - 1] == '!' || str[i - 1] == '?'
-		|| str[i - 1] == '"' || str[i - 1] == '('
-		|| str[i - 1] == ')' || str[i - 1] == '{'
-		|| str[i - 1] == '}' || str[i - 1] == '0')
-			str[i] -= 32;
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			index++;
+	if (str[i - 1] == ' ' || str[i - 1] == '\t'
+	|| str[i - 1] == '\n' || str[i - 1] == ','
+	|| str[i - 1] == ';' || str[i - 1] == '.'
+	|| str[i - 1] == '!' || str[i - 1] == '?'
+	|| str[i - 1] == '"' || str[i - 1] == '('
+	|| str[i - 1] == ')' || str[i - 1] == '{'
+	|| str[i - 1] == '}' || str[i - 1] == '0')
+		str[i] -= 32;
 		i++;
 	}
+
 	return (str);
 }
