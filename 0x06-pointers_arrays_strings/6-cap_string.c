2bcf0b8d1d2622afc38fa42bafb2fcd4037bc1f2
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-03-02 11:20:59 -0800

    function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,15 +11,15 @@ char *cap_string(char *str)
 	int i = 0;
 
 	for (i = 0; str[i]; i++)
-	for (; str[i] >= 'a' && str[i] <= 'z'; i++)
+	for (i = i; str[i] >= 'a' && str[i] <= 'z'; i++)
 	{
-		if (str[i - 1] == ' ' || str[i - 1] == '\t')
-		if (str[i - 1] == '\n' || str[i - 1] == ',')
-		if (str[i - 1] == ';' || str[i - 1] == '.')
-		if (str[i - 1] == '!' || str[i - 1] == '?')
-		if (str[i - 1] == '"' || str[i - 1] == '(')
-		if (str[i - 1] == ')' || str[i - 1] == '{')
-		if (str[i - 1] == '}' || str[i - 1] == '0')
+		if (str[i - 1] == ' ' || str[i - 1] == '\t'
+		|| str[i - 1] == '\n' || str[i - 1] == ','
+		|| str[i - 1] == ';' || str[i - 1] == '.'
+		|| str[i - 1] == '!' || str[i - 1] == '?'
+		|| str[i - 1] == '"' || str[i - 1] == '('
+		|| str[i - 1] == ')' || str[i - 1] == '{'
+		|| str[i - 1] == '}' || str[i - 1] == '0')
 			str[i] -= 32;
 	}
 	return (str);
