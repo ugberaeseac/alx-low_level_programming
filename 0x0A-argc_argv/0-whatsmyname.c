0be25fd3b211e50af5b7f76941df151498f862f6
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-03-10 09:57:03 -0800

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,12 @@
 /**
  * main - print the program name
  * @argc: argument count
- * kii@argv: argument vector
+ * @argv: argument vector
  *
  * Return: 0 if success
  */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc * 0]);
-	return(0);
+	return (0);
 }
