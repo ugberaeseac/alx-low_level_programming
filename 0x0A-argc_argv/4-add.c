80b4d9b56fb361eb2ce9c41276a09527c70c9ab1
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-03-10 17:08:31 -0800

    A program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include "main.h"
 
 /**
  * main - program that adds positive number
@@ -11,26 +12,47 @@
  */
 int main(int argc, char *argv[])
 {
-	 int i;
-	 int sum = 0;
+	int j;
+	int sum = 0;
 
 	if (argc < 2)
-	{
-		printf("0\n");
-	}
+        {
+                printf("0\n");
+		return (0);
+        }
 
-	for (i = 1; i < argc; i++)
-	{
-		if (argc > 1)
-		{
-			sum += atoi(argv[i]);
-		}
-		if (!(isdigit(argv[i] == 0)))
-		{
-			printf("Error\n");
+        for (j = 1; j < argc; j++)
+        {
+                if ((_isdigits(argv[j])))
+                {
+                        sum += atoi(argv[j]);
+                }
+                else
+                {
+                        printf("Error\n");
 			return (1);
+                }
+        }
+        printf("%d\n", sum);
+        return (0);
+}
+
+/**
+ * _isdigits - function to check if arguement passed is a digit
+ * @str: argument to check
+ *
+ * Return: 0 success 1 if not
+ */
+int _isdigits (char *str)
+{
+	int i;
+	
+	for (i = 0; str[i]; i++)
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+		{
+			return (0);
 		}
 	}
-	printf("%d\n", sum);
-	return (0);
+	return (1);
 }
