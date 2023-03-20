c1aef4aa1423777eb73b074c88fa8dc85fb0f411
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-03-20 16:04:01 -0700

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory represents my understanding on 0x0E. C - Structures, typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new type struct dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: A user defined datatype that combine
+ * data items of different kinds.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
