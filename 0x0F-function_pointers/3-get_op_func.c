814487cacdae9ba8ed91a2f3f260b17c61bb8b50
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-03-22 16:08:12 -0700

    A program that perform simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <stdlib.h>
 
 /**
  * get_op_func - function that selects the correct function to
@@ -9,4 +10,21 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	
+	int i;
 
+	for (i = 0; ops[i].op != NULL; i++)
+	{
+		if (s == ops[i].op)
+			return (ops[i].f);
+	}
+	return (NULL);
+}
