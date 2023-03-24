55a92136d6f0ba0af0c72fa902e647a2f9008571
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-03-23 17:47:20 -0700

    A function that prints anything.

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,18 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints any thing
+ * @format: list of arguments passed to the function
+ *
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	va_list print;
+	char *s;
+	int i = 0;
+
+	
+
+
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,5 +7,17 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct - struct
+ * @character: character format
+ * @fptr: function pointer
+ */
+typedef struct 
+{
+	char *character;
+	void (*fptr)();
+} datatype;
 
 #endif
