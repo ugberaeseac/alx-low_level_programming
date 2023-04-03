31045a5e78693de8fce0ddb644c7c25afe2b899c
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-04-03 06:56:14 -0700

    A function that adds a new node at the end of a singly list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,11 +11,11 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node, *temp;
-	
+
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->n = n;
 	new_node->next = (NULL);
 
@@ -32,6 +32,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 	temp->next = new_node;
 
-	return(new_node);
+	return (new_node);
 }
 
