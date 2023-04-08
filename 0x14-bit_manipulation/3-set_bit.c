d08b67860f35de4ecc6a27982c8167b487e07f1c
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-04-08 16:06:02 -0700

    edited to total  number of bits rather than length of binary number

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,19 +9,11 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int temp;
-	unsigned long int base_length = 0;
+	unsigned long int total_bits;
 
-	temp = *n;
+	total_bits = (sizeof(unsigned long int) * 8);
 
-	while (temp > 0)
-	{
-		temp = temp >> 1;
-		base_length++;
-	}
-	base_length = base_length - 1;
-
-	if (index > base_length)
+	if (index > total_bits)
 	{
 		return (-1);
 	}
