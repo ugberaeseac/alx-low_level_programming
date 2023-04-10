b37ddde4501951667b34dfe58cc7ef708f0934b1
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-04-10 11:59:08 -0700

    check if text_content is NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 
 	for (i = 0; text_content[i] != '\0'; i++)
 		strlength++;
@@ -28,7 +34,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	buffer = text_content;
 
-	ret_write = write(fd, buffer, (strlength + 1));
+	ret_write = write(fd, buffer, strlength);
 	if (ret_write == -1)
 	{
 		free(buffer);
