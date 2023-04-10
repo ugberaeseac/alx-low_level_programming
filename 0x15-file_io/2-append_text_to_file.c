10381b13274763e199ee8592be4d293731e95a35
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-04-10 13:50:14 -0700

    debugg return value wh no permission to write to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -30,7 +30,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		strlength++;
 
 	write_n = write(fd, text_content, strlength);
-	if (write_n == -1)
+	if (write_n == -1 || write_n != strlength)
 	{
 		close(fd);
 		return (-1);
