c77cf635fd92f5038f2d8d25ebb1c8ced50c525b
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-04-10 11:53:28 -0700

    edit number of bytes to write on write system call function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	buffer = text_content;
 
-	ret_write = write(fd, buffer, (sizeof(buffer));
+	ret_write = write(fd, buffer, (strlength + 1));
 	if (ret_write == -1)
 	{
 		free(buffer);
