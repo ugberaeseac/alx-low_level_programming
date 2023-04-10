d20f0750875960a50ebf5369f0ab16f069af3e90
Ugberaese Amowie Charles <ugberaeseac@gmail.com>
2023-04-10 11:51:45 -0700

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	buffer = text_content;
 
-	ret_write = write(fd, buffer, strlength);
+	ret_write = write(fd, buffer, (sizeof(buffer));
 	if (ret_write == -1)
 	{
 		free(buffer);
